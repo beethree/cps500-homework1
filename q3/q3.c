c802c28246ab84eff3aecf9e06cb27c61590aa7a
root <root@DESKTOP-M6JP7P2>
2023-07-26 21:49:40 -0500

    early q3 commit, not working

--- q3/q3.c
+++ q3/q3.c
@@ -4,9 +4,37 @@
 
 #include <stdio.h>
 
+int fibonacci(int n) 
+{
+   // TODO: Debug delete
+   //printf("The number passed to the function is: %d \n", n);
+
+   //Fibonacci sequence is F(n) = F(n-1) + F(n-2)
+   int output = ((n - 1) + (n - 2));
+
+   return output;
+}
+
 int main(int argc, char *argv[])
 {
-   printf("number of command line arguments entered: %d \n", argc);
+   // cast the input 'char' value to an int for processing
    int input = *argv[argc-1] - '0';
    printf("The passed in argument is: %d \n", input); 
+
+   // loop through the input processing as Fibonacci sequence
+   for(int i = 0; i <= input; i++)
+   {
+      if (i == 0)
+      {
+	 printf("%d : 0 \n", i);
+      }
+      else if (i == 1)
+      {
+	 printf("%d : 1 \n", i);
+      }
+      else
+      {
+	 printf("%d : %d \n", i, fibonacci(i));
+      }
+   }
 }
