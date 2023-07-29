d16fd96f97cf9bce5e90320406ed9d47fbde5bb8
root <root@DESKTOP-M6JP7P2>
2023-07-28 21:28:53 -0500

    early stages of q4

--- q4/q4.c
+++ q4/q4.c
@@ -4,9 +4,42 @@
 
 #include <stdio.h>
 
-int main()
+void arrayAddition(int A[], int B[], int N, int O[])
+{
+   
+   //print O - calcuated value of element wise addition of A[] & B[]
+   for(int i = 0; i < N; i++)
+   {
+      O[i] = A[i] + B[i];
+      printf("%d ", O[i]);
+   }
+
+   printf("\n \n");
+}
+
+int main(int argc, char *argv[])
 {
 
+   // TODO: set up method to obtain array contents via command line
+   int A[] = {2,4,6,8,10};
+   int B[] = {1,1,1,1,1};
+   int N = sizeof(A) / sizeof(A[0]);
+   int Z = sizeof(B) / sizeof(B[0]);
 
+   printf("The size of Array A = %d \n", N);
+   printf("The size of Array B = %d \n", Z);
+
+   if (N == Z)
+   {
+      // set size of array O
+      int O[N];
+
+      // pass inputs to function for addition
+      arrayAddition(A, B, N, O);
+   }
+   else
+   {
+      printf("Please try again with array sizes that equal. \n");
+   }
 
 }
