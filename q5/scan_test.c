3f84b2fb6b402f7626e3f35008a3b14811f932f6
root <root@DESKTOP-M6JP7P2>
2023-07-29 18:05:02 -0500

    finalized test program

--- q5/scan_test.c
+++ q5/scan_test.c
@@ -3,27 +3,32 @@
 
 int main()
 {
-  printf("Please enter a integer, an operation, and another integer. Just hit enter to quit. \n");
+  printf("Please enter a integer, an operation, and another integer. Hit enter to quit. \n");
+  char input[4];
   int a;
   char operation;
   int b;
-  char input[4];
-  //sscanf(input, "%d %c %d", a, operation, b);
-  //printf("%d %c %d", a, operation, b);
 
-  // used fgets() instead of gets() because of the warning from Ubuntu and gcc
+  // used fgets() instead of gets() because of the warnings from both Ubuntu and gcc
   fgets(input,4,stdin);
 
+  // determine if user input a blank line to quit
   if (input[0] == '\n')
   {
-     printf("screw up \n");
+     printf("Exiting! \n");
   }
-  else
+  else // no blank line, so proceed
   {
      printf("You entered: %s \n", input);
 
+     // peel values from array while casting char(s) to int(s)
+     a = (input[0] - '0');
+     operation = input[1];
+     b = (input [2] - '0');
+
+     printf("a, operation, and b: %d, %c, %d \n", a, operation, b);
+
   }
-  //sscanf(input, "%*d %*s %*d", a, operation, b);
 
   return(0);
 }
