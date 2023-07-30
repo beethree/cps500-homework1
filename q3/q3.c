0f12624e071e707ac5c2a195f8748684312c7f54
root <root@DESKTOP-M6JP7P2>
2023-07-29 21:34:27 -0500

    ready to turn in

--- q3/q3.c
+++ q3/q3.c
@@ -6,13 +6,15 @@
 
 int fibonacci(int n) 
 {
-   // TODO: Debug delete
-   //printf("The number passed to the function is: %d \n", n);
-
-   //Fibonacci sequence is F(n) = F(n-1) + F(n-2)
-   int output = ((n - 1) + (n - 2));
-
-   return output;
+   if (n == 0 || n == 1)
+   {
+      return 1;
+   }
+   else
+   {
+      //Fibonacci sequence is F(n) = F(n-1) + F(n-2)
+      return fibonacci(n - 1) + fibonacci(n - 2);
+   }
 }
 
 int main(int argc, char *argv[])
@@ -24,17 +26,6 @@ int main(int argc, char *argv[])
    // loop through the input processing as Fibonacci sequence
    for(int i = 0; i <= input; i++)
    {
-      if (i == 0)
-      {
-	 printf("%d : 0 \n", i);
-      }
-      else if (i == 1)
-      {
-	 printf("%d : 1 \n", i);
-      }
-      else
-      {
-	 printf("%d : %d \n", i, fibonacci(i));
-      }
+      printf("%d : %d \n", i, fibonacci(i));
    }
 }
