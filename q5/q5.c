1e27d880232c5d90380532219e35f259f2c0ce40
root <root@DESKTOP-M6JP7P2>
2023-07-29 21:07:58 -0500

    ready to turn in

--- q5/q5.c
+++ q5/q5.c
@@ -3,6 +3,7 @@
 //Homework 1, Question 5
 
 #include <stdio.h>
+#include <stdbool.h>
 
 int calculate(int a, int b, char operation)
 {
@@ -39,29 +40,37 @@ int main()
    int a;
    int b;
    char op;
-   char input[4]; //user input
+   char input[5]; //user input
+   bool test = true;
 		 
-   while (input[0] != '\n')
+   while (test)
    {
       printf("Please enter an integer, an operation, and another integer. Hit enter to quit. \n");
 
-      // capture user input (use fgets() vs. gets() due to Ubuntu and gcc warnings
-      fgets(input,4,stdin); 
-			    
-      // take char array inputs assign to variables, casting where necessary
-      a  = (input[0] - '0');
-      op = input[1];
-      b  = (input[2] - '0');
+      // capture user input (use fgets() vs. gets() due to Ubuntu and gcc warnings)
+      fgets(input,5,stdin); 
+
+      // user has entered a blank line
+      if (input[0] == '\n')
+      {
+	 // set condition to false
+	 test = 0;
+
+	 // user has entered blank line, exit
+	 printf("Exiting. \n");
+      }
+      else // proceed with processing
+      {
+	 // take char array inputs assign to variables, casting where necessary
+	 a  = (input[0] - '0');
+	 op = input[1];
+	 b  = (input[2] - '0');
       
-      printf("You entered a, op, b : %d and %c and %d \n", a, op, b);
-
-      // return calculation to user
-      printf("Your answer: %d \n", calculate(a,b,op));
+	 // return calculation to user
+	 printf("%d \n", calculate(a,b,op));
+      }
    }
-
-   // user has entered blank line, exit
-   printf("Exiting.");
-
-
-   return(0);
 }
+
+
+			    
