6215939db93547ddfecabbedc6ba4afe38a7674a
root <root@DESKTOP-M6JP7P2>
2023-07-29 20:29:41 -0500

    not working while loop

--- q5/q5.c
+++ q5/q5.c
@@ -4,7 +4,7 @@
 
 #include <stdio.h>
 
-void calculate(int a, int b, char operation)
+int calculate(int a, int b, char operation)
 {
    int output = 0;
 
@@ -30,8 +30,7 @@ void calculate(int a, int b, char operation)
 	 printf("invalid operator provided \n");
 	 break;
    }
-
-   printf("Your answer: %d \n", output);
+   return output;
 }
 
 int main()
@@ -42,26 +41,27 @@ int main()
    char op;
    char input[4]; //user input
 		 
-   printf("Please enter an integer, an operation, and another integer. Hit enter to quit. \n");
-
-   fgets(input,4,stdin); // capture user input (use fgets vs. gets due to Ubuntu and gcc warnings)
-
-   if (input[0] == 'n')
+   while (input[0] != '\n')
    {
-      // user has entered blank line, exit
-      printf("Exiting.");
-   }
-   else
-   {
+      printf("Please enter an integer, an operation, and another integer. Hit enter to quit. \n");
+
+      // capture user input (use fgets() vs. gets() due to Ubuntu and gcc warnings
+      fgets(input,4,stdin); 
+			    
       // take char array inputs assign to variables, casting where necessary
       a  = (input[0] - '0');
       op = input[1];
       b  = (input[2] - '0');
       
-      printf("You entered a, operation, b : %d and %c and %d \n", a, op, b);
+      printf("You entered a, op, b : %d and %c and %d \n", a, op, b);
 
-      calculate(a, b, op);
+      // return calculation to user
+      printf("Your answer: %d \n", calculate(a,b,op));
    }
+
+   // user has entered blank line, exit
+   printf("Exiting.");
+
 
    return(0);
 }
