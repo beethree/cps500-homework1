ea9d4d7d92685358b102b0e8fe08b1b4efc895cc
root <root@DESKTOP-M6JP7P2>
2023-07-29 20:03:52 -0500

    if statement working version

--- q5/q5.c
+++ q5/q5.c
@@ -36,15 +36,32 @@ void calculate(int a, int b, char operation)
 
 int main()
 {
-   // set up and request user input
+   // set up to handle user input and capture for processing
    int a;
    int b;
-   char operation;
+   char op;
+   char input[4]; //user input
+		 
+   printf("Please enter an integer, an operation, and another integer. Hit enter to quit. \n");
 
-   printf("Please enter an integer, followed by an operation, and another integer: \n");
-   scanf("%d%c%d", &a, &operation, &b);
+   fgets(input,4,stdin); // capture user input (use fgets vs. gets due to Ubuntu and gcc warnings)
 
-   printf("You entered: %d and %c and %d \n", a, operation, b);
+   if (input[0] == 'n')
+   {
+      // user has entered blank line, exit
+      printf("Exiting.");
+   }
+   else
+   {
+      // take char array inputs assign to variables, casting where necessary
+      a  = (input[0] - '0');
+      op = input[1];
+      b  = (input[2] - '0');
+      
+      printf("You entered a, operation, b : %d and %c and %d \n", a, op, b);
 
-   calculate(a, b, operation);
+      calculate(a, b, op);
+   }
+
+   return(0);
 }
