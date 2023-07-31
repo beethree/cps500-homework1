2884a784361ee13e523a112d6da435b0ba454fdc
root <root@DESKTOP-M6JP7P2>
2023-07-30 21:17:05 -0500

    working, but seg faults

--- q4/q4.c
+++ q4/q4.c
@@ -3,28 +3,64 @@
 //Homework 1, Question 4
 
 #include <stdio.h>
+#include <stdlib.h>
 
 void arrayAddition(int A[], int B[], int N, int O[])
 {
-   
    //print O - calcuated value of element wise addition of A[] & B[]
    for(int i = 0; i < N; i++)
    {
       O[i] = A[i] + B[i];
       printf("%d ", O[i]);
    }
-
-   printf("\n \n");
 }
 
 int main(int argc, char *argv[])
 {
+   int input_count = argc;
+   printf("input count = %d \n", argc);
+   int input_counter;
 
-   // TODO: set up method to obtain array contents via command line
-   int A[] = {2,4,6,8,10};
-   int B[] = {1,1,1,1,1};
-   int N = sizeof(A) / sizeof(A[0]);
-   int Z = sizeof(B) / sizeof(B[0]);
+   int A[input_count -1];
+   int B[input_count -1];
+
+   // fill array A from the command line 
+   for (int i = 0; i < ((input_count/2)); i++)
+   {
+      input_counter = 0;
+
+      if (*argv[i+1] == '+')
+      {
+	 printf("\n+ reached! \n");
+	 input_counter += i;
+	 printf("input counter %d : \n", input_counter); 
+	 break;
+      }
+      else
+      {
+	 A[i] = atoi(argv[i+1]);
+	 printf("%d ", A[i]);
+	 input_counter += i;
+      }
+   }
+
+   // fill array B from the command line
+   // note that outer for loop does not increment until inside inner for loop
+   for (int j = input_counter + 2; j < input_count;)
+   {
+      //printf("j = %d \n", j);
+      for (int k = 0; k < ((input_count/2)-1); k++)
+      {
+         B[k] = atoi(argv[j]);
+	 j++; // advance j after reading in to array B
+         printf("%d ", B[k]);
+      }
+   }
+
+   int N = ((sizeof(A) / sizeof(A[0]))/2);
+   int Z = ((sizeof(B) / sizeof(B[0]))/2);
+
+   printf("\n\n");
 
    printf("The size of Array A = %d \n", N);
    printf("The size of Array B = %d \n", Z);
@@ -42,4 +78,5 @@ int main(int argc, char *argv[])
       printf("Please try again with array sizes that equal. \n");
    }
 
+   printf("\n\n");
 }
